07370d8befbd27722596537d9fa191bb76c2ca6b
Sanshui2 <1540647428@qq.com>
2025-08-20 17:55:42 +0800

    Assignment1

---
+++ main.cpp
@@ -0,0 +1,163 @@
+#include "Triangle.hpp"
+#include "rasterizer.hpp"
+#include <eigen3/Eigen/Eigen>
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+constexpr double MY_PI = 3.1415926;
+
+Eigen::Matrix4f get_view_matrix(Eigen::Vector3f eye_pos)
+{
+    Eigen::Matrix4f view = Eigen::Matrix4f::Identity();
+
+    Eigen::Matrix4f translate;
+    translate << 1, 0, 0, -eye_pos[0], 0, 1, 0, -eye_pos[1], 0, 0, 1,
+        -eye_pos[2], 0, 0, 0, 1;
+
+    view = translate * view;
+
+    return view;
+}
+
+Eigen::Matrix4f get_model_matrix(float rotation_angle)
+{
+    Eigen::Matrix4f model = Eigen::Matrix4f::Identity();
+
+ 
+
+    // TODO: Implement this function
+    // Create the model matrix for rotating the triangle around the Z axis.
+    // Then return it.
+
+    //std::sin / std::cos 接受弧度
+    float rad = rotation_angle * MY_PI / 180.0f; // 角度→弧度
+
+    float c = std::cos(rad), s = std::sin(rad);
+
+    model << c, -s, 0, 0,
+        s, c, 0, 0,
+        0, 0, 1, 0,
+        0, 0, 0, 1;
+
+    return model;
+}
+
+Eigen::Matrix4f get_projection_matrix(float eye_fov, float aspect_ratio,
+                                      float zNear, float zFar)
+{
+    // Students will implement this function
+
+    Eigen::Matrix4f projection = Eigen::Matrix4f::Identity();
+
+    // TODO: Implement this function
+    // Create the projection matrix for the given parameters.
+    // Then return it.
+        //把透视投影写成矩阵
+    Eigen::Matrix4f perspective;
+    float tanfov = std::tan(eye_fov / 2 * MY_PI / 180.0f); // 角度→弧度
+    perspective << 1 / (aspect_ratio * tanfov), 0, 0, 0,
+        0, 1 / tanfov, 0, 0,
+        0, 0, -((zFar + zNear) / (zFar - zNear)), -(2 * zFar * zNear) / (zFar - zNear),
+        0, 0, -1, 0;
+
+    projection = perspective * projection;
+
+    return projection;
+}
+
+Eigen::Matrix4f get_rotation(Vector3f axis, float angle)
+{
+    axis.normalize();
+
+    angle =angle/180.0f * MY_PI; // 角度→弧度
+
+    Eigen::Matrix3f I = Eigen::Matrix3f::Identity();
+    Eigen::Matrix3f N;
+
+    N << 0, -axis.z(), axis.y(),
+        axis.z(), 0, -axis.x(),
+        -axis.y(), axis.x(), 0;
+
+    Eigen::Matrix3f rodrigous = cos(angle) * I + (1 - cos(angle)) * axis * axis.transpose() + sin(angle) * N;
+
+    Eigen::Matrix4f rotation = Eigen::Matrix4f::Identity();
+
+    rotation.block<3, 3>(0, 0) = rodrigous;
+
+    return rotation;
+}
+
+
+int main(int argc, const char** argv)
+{
+    float angle = 0;
+    bool command_line = false;
+    std::string filename = "output.png";
+
+    if (argc >= 3) {
+        command_line = true;
+        angle = std::stof(argv[2]); // -r by default
+        if (argc == 4) {
+            filename = std::string(argv[3]);
+        }
+    }
+
+    rst::rasterizer r(700, 700);
+
+    Eigen::Vector3f eye_pos = {0, 0, 5};
+
+    std::vector<Eigen::Vector3f> pos{{2, 0, -2}, {0, 2, -2}, {-2, 0, -2}};
+
+    std::vector<Eigen::Vector3i> ind{{0, 1, 2}};
+
+	Eigen::Vector3f axis = { 20, 0, 30 }; 
+
+    auto pos_id = r.load_positions(pos);
+    auto ind_id = r.load_indices(ind);
+
+    int key = 0;
+    int frame_count = 0;
+
+    if (command_line) {
+        r.clear(rst::Buffers::Color | rst::Buffers::Depth);
+
+        r.set_model(get_model_matrix(angle));
+        r.set_view(get_view_matrix(eye_pos));
+        r.set_projection(get_projection_matrix(45, 1, 0.1, 50));
+
+        r.draw(pos_id, ind_id, rst::Primitive::Triangle);
+        cv::Mat image(700, 700, CV_32FC3, r.frame_buffer().data());
+        image.convertTo(image, CV_8UC3, 1.0f);
+
+        cv::imwrite(filename, image);
+
+        return 0;
+    }
+
+    while (key != 27) {
+        r.clear(rst::Buffers::Color | rst::Buffers::Depth);
+
+        //r.set_model(get_model_matrix(angle));
+        r.set_model(get_rotation(axis,angle));
+        r.set_view(get_view_matrix(eye_pos));
+        r.set_projection(get_projection_matrix(45, 1, 0.1, 50));
+
+        r.draw(pos_id, ind_id, rst::Primitive::Triangle);
+
+        cv::Mat image(700, 700, CV_32FC3, r.frame_buffer().data());
+        image.convertTo(image, CV_8UC3, 1.0f);
+        cv::imshow("image", image);
+        key = cv::waitKey(10);
+
+        std::cout << "frame count: " << frame_count++ << '\n';
+
+        if (key == 'a') {
+            angle += 10;
+        }
+        else if (key == 'd') {
+            angle -= 10;
+        }
+    }
+
+    return 0;
+}
